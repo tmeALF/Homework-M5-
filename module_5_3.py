569eef776f408c1cec3f0f8d4b4befe96dc8fa95
tmeALF <m1160955@yandex.ru>
2024-08-23 14:46:49 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -47,13 +47,10 @@ class House:
             return self
 
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        return self.__add__(value)
 
     def __iadd__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
-            return self
+        return self.__add__(value)
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -62,20 +59,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
